d06b31b262704a68a08f2a1d3a72d93a9c36c9bd
Xiao Han <hanx7@student.unimelb.edu.au>
2024-07-29 23:34:59 +1000

    modify some css

--- my-portfolio/src/pages/contact.tsx
+++ my-portfolio/src/pages/contact.tsx
@@ -12,7 +12,7 @@ function ContactPage() {
         //     className="mx-auto my-16 flex flex-col items-center justify-center gap-4 px-2 md:my-20  md:max-w-full lg:flex-row lg:items-start lg:gap-16"
         // >
 
-        <section className="flex flex-col items-center bg-[#0b0d26]">
+        <section className="min-h-screen flex flex-col items-center bg-[#0b0d26]">
             <ResponsiveAppBar pages={["About me", "Contact me"]}></ResponsiveAppBar>
             <h1 className="mb-4 text-[2rem] md:text-[4rem] text-white">
                 <MotionText delayOffset={0}>Contact me</MotionText>
